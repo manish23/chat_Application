7e0bf14834fbf7e7ea0a998512719eefc09800cf
yuejinzhao <lvgn13@mails.tsinghua.edu.cn>
2015-09-04 22:16:21 +0800

    conflict

---
+++ app/src/main/java/com/ihs/demo/message_2013011337/chat_activity.java
@@ -0,0 +1,48 @@
+package com.ihs.demo.message_2013011337;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.ihs.message_2013011337.R;
+
+public class chat_activity extends Activity {
+    private  String name;
+    private  String mid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.activity_chat_activity);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        mid = intent.getStringExtra("mid");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
